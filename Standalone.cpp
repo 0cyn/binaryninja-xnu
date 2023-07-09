6ef9cf69309285b43516b230c40d6f0be5aa1dd4
kat <kat@vector35.com>
2023-07-09 07:09:02 -0400

    Init

---
+++ Standalone.cpp
@@ -0,0 +1,37 @@
+//
+// Created by kat on 7/9/23.
+//
+
+#include <binaryninjaapi.h>
+
+// Core
+#include "Workflows/DarwinKernel.h"
+
+#ifdef UI_BUILD
+#include "uitypes.h"
+#include "UI/TypeSetter.h"
+#endif
+
+
+extern "C" {
+
+BN_DECLARE_CORE_ABI_VERSION
+
+#ifdef UI_BUILD
+BN_DECLARE_UI_ABI_VERSION
+#endif
+
+BINARYNINJAPLUGIN bool CorePluginInit() {
+
+    DarwinKernelWorkflow::Register();
+
+    return true;
+}
+#ifdef UI_BUILD
+BINARYNINJAPLUGIN bool UIPluginInit() {
+
+    return true;
+}
+#endif
+
+}
